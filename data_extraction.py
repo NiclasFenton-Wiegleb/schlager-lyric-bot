feb3b39e147b9526ca9c53ef2abb5918638981c8
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2023-04-13 21:53:39 +0100

    Establishing pipeline for retrieving playlist data, isolating song info and appending it to an existing csv file.

--- data_extraction.py
+++ data_extraction.py
@@ -8,6 +8,8 @@ class DataExtractor:
         pass
 
     def retrieve_playlist_content(self, playlist_id):
+
+        #API request to retrieve playlist data
         url = "https://spotify-scraper.p.rapidapi.com/v1/playlist/contents"
 
         querystring = {"playlistId":f"{playlist_id}"}
@@ -19,27 +21,54 @@ class DataExtractor:
 
         response = requests.request("GET", url, headers=headers, params=querystring)
 
+        #Save playlist content as json file
         playlist_content = response.json()
 
         with open(f"{playlist_id}_content.json", "w") as f:
             json.dump(playlist_content, f)
         
+    def playlist_to_song_data(self, playlist_id):
+        #Note: retrieve_playlist_content() method needs to be run beforehand
+
+        #Open json file with playlist content
+        f = open(f"{playlist_id}_content.json")
+
+        playlist_content = json.load(f)
+
+        #Extract relevant song info from playlist content
+        url = [x["shareUrl"] for x in playlist_content["contents"]["items"]]
+        song_name = [x["name"] for x in playlist_content["contents"]["items"]]
+        song_id = [x["id"] for x in playlist_content["contents"]["items"]]
+        release_year = [x["album"]["date"] for x in playlist_content["contents"]["items"]]
+
+        #Convert song data to dataframe
+        song_dict = {"url": url,
+                    "title": song_name,
+                    "spotify_id": song_id,
+                    "release_year": release_year}
+
+        df = pd.DataFrame(song_dict)
+        return df
+        
 
 
-#extractor = DataExtractor()
+extractor = DataExtractor()
 
-playlist_id = "2dCNbxILFEJnnEb8h6L4eV"
+playlist_id = "6R41RrIjNVNVvPGziXs9F8" 
 
 #playlist = extractor.retrieve_playlist_content(playlist_id)
 
+df = extractor.playlist_to_song_data(playlist_id)
 
-f = open(f"{playlist_id}_content.json")
+#df.to_csv("schlager_songs.csv", mode= "a", index= False, header= False)
 
-playlist_content = json.load(f)
+songs = pd.read_csv("schlager_songs.csv")
 
-playlist_song_ids = [x["id"] for x in playlist_content["contents"]["items"]]
 
-print(playlist_song_ids)
+print(songs)
+print(songs.info())
+
+
 
 #for x in playlist_content:
     #playlist_song_ids.append()
