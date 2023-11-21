1fc9f3e9e2e418dcbe1687291a1334e3a2b525a9
Niclas Fenton-Wiegleb <niclas.wiegleb@gmail.com>
2023-11-21 09:46:40 +0000

    Removing unnecessary files and folders.

--- upload_tensorboard.py
+++
@@ -1,4 +0,0 @@
-# !tensorboard dev upload --logdir ./logs \
-#   --name "Simple experiment with MNIST" \
-#   --description "Training results from https://colab.sandbox.google.com/github/tensorflow/tensorboard/blob/master/docs/tbdev_getting_started.ipynb" \
-#   --one_shot
